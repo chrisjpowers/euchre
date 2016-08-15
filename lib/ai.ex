073c87a394946c356ea58f8062f427008ea4d5af
Chris Powers <chrisjpowers@gmail.com>
2016-08-15 11:32:25 -0500

    Added trick taking logic in suit

--- lib/ai.ex
+++ lib/ai.ex
@@ -3,6 +3,38 @@ defmodule Euchre.Ai do
 
   def choose_card(trump, played_cards, hand) do
     {lead_suit, _} = List.first(played_cards)
+    rules = [
+      &play_winner_in_lead_suit/4,
+      &throw_off_lowest_card/4,
+      &throw_off_lowest_offsuit_card/4
+    ]
+    rules |>
+      Enum.map(fn (rule) ->
+        rule.(trump, lead_suit, played_cards, hand)
+      end) |>
+      Enum.find &(&1)
+  end
+
+  defp play_winner_in_lead_suit(trump, lead_suit, played, hand) do
+    cards = cards_matching_suit(hand, lead_suit)
+    winners = Enum.filter(cards, fn (card) ->
+      card == Trick.winner(trump, played ++ [card])
+    end)
+    if length(winners) > 0 do
+      Trick.lowest_card(winners, lead_suit, trump) 
+    end
+  end
+
+  defp throw_off_lowest_card(trump, lead_suit, _played, hand) do
+    cards = cards_matching_suit(hand, lead_suit)
+    List.first(cards)
+  end
+
+  defp throw_off_lowest_offsuit_card(trump, lead_suit, _played, hand) do
     Trick.lowest_card(hand, lead_suit, trump)
+  end
+
+  defp cards_matching_suit(hand, lead_suit) do
+    Enum.filter hand, fn ({suit, _}) -> suit == lead_suit end
   end
 end
--- test/ai_test.exs
+++ test/ai_test.exs
@@ -3,7 +3,7 @@ defmodule AiTest do
 
   alias Euchre.Ai
 
-  test "throws off lowest non-trump when can't follow suit" do
+  test "throws off lowest card when can't follow suit" do
     played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
     hand = [
       {"diamonds", "9"},
@@ -14,5 +14,44 @@ defmodule AiTest do
     ]
     result = Ai.choose_card("clubs", played_cards, hand)
     assert result == {"diamonds", "9"}
+  end
+
+  test "follows suit when one card that matches" do
+    played_cards = [{"hearts", "A"}, {"hearts", "10"}, {"hearts", "J"}]
+    hand = [
+      {"diamonds", "9"},
+      {"diamonds", "10"},
+      {"diamonds", "J"},
+      {"hearts", "Q"},
+      {"diamonds", "K"}
+    ]
+    result = Ai.choose_card("clubs", played_cards, hand)
+    assert result == {"hearts", "Q"}
+  end
+
+  test "takes the trick if it can with one of two cards" do
+    played_cards = [{"hearts", "K"}, {"hearts", "10"}, {"hearts", "J"}]
+    hand = [
+      {"diamonds", "9"},
+      {"diamonds", "10"},
+      {"diamonds", "J"},
+      {"hearts", "Q"},
+      {"hearts", "A"}
+    ]
+    result = Ai.choose_card("clubs", played_cards, hand)
+    assert result == {"hearts", "A"}
+  end
+
+  test "takes the trick with lowest card possible in last position" do
+    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
+    hand = [
+      {"diamonds", "9"},
+      {"diamonds", "10"},
+      {"diamonds", "J"},
+      {"hearts", "A"},
+      {"hearts", "Q"}
+    ]
+    result = Ai.choose_card("clubs", played_cards, hand)
+    assert result == {"hearts", "Q"}
   end
 end
