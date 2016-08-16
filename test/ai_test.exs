dfdc5fdb388f85cee352bf9f1ba2c04ea558e363
Chris Powers <chrisjpowers@gmail.com>
2016-08-16 07:57:55 -0500

    Refactored Ai tests for brevity

--- test/ai_test.exs
+++ test/ai_test.exs
@@ -3,133 +3,64 @@ defmodule AiTest do
 
   alias Euchre.Ai
 
+  def choose(trump_code, played_codes, hand_codes) do
+    played_cards = Enum.map played_codes, &code_to_card/1
+    hand = Enum.map hand_codes, &code_to_card/1
+    result = Ai.choose_card(code_to_suit(trump_code), played_cards, hand)
+    card_to_code(result)
+  end
+
+  def code_to_card(code) do
+    [_, value, suit_code] = Regex.run(~r/^(.{1,2})(\w)$/, code) 
+    {code_to_suit(suit_code), value}
+  end
+
+  def card_to_code({suit, value}) do
+    "#{value}#{String.first suit}"
+  end
+
+  def code_to_suit("c"), do: "clubs"
+  def code_to_suit("d"), do: "diamonds"
+  def code_to_suit("h"), do: "hearts"
+  def code_to_suit("s"), do: "spades"
+
   test "throws off lowest card when can't follow suit" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"diamonds", "9"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"diamonds", "Q"},
-      {"diamonds", "K"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"diamonds", "9"}
+    assert choose("c", ~w(9h 10h Jh), ~w(9d 10d Jd Qd Kd)) == "9d"
   end
 
   test "follows suit when one card that matches" do
-    played_cards = [{"hearts", "A"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"diamonds", "9"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"hearts", "Q"},
-      {"diamonds", "K"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"hearts", "Q"}
+    assert choose("c", ~w(Ah 10h Jh), ~w(9d 10d Jd Qh Kd)) == "Qh"
   end
 
   test "takes the trick if it can with one of two cards" do
-    played_cards = [{"hearts", "K"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"diamonds", "9"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"hearts", "Q"},
-      {"hearts", "A"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"hearts", "A"}
+    assert choose("c", ~w(Kh 10h Jh), ~w(9d 10d Jd Qh Ah)) == "Ah"
   end
 
   test "takes the trick with lowest card possible in last position" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"diamonds", "9"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"hearts", "A"},
-      {"hearts", "Q"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"hearts", "Q"}
+    assert choose("c", ~w(9h 10h Jh), ~w(9d 10d Jd Ah Qh)) == "Qh"
   end
 
   test "must play left bauer to follow suit" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"diamonds", "9"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("hearts", played_cards, hand)
-    assert result == {"diamonds", "J"}
+    assert choose("h", ~w(9h 10h Jh), ~w(9d 10d Jd Ad Qd)) == "Jd"
   end
 
   test "trump with the sole trump card if in last position" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"spades", "K"},
-      {"diamonds", "10"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("spades", played_cards, hand)
-    assert result == {"spades", "K"}
+    assert choose("s", ~w(9h 10h Jh), ~w(Ks 10d Jd Ad Qd)) == "Ks"
   end
 
   test "trump with the lowest trump card if in last position" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"spades", "A"},
-      {"spades", "K"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("spades", played_cards, hand)
-    assert result == {"spades", "K"}
+    assert choose("s", ~w(9h 10h Jh), ~w(As Ks Jd Ad Qd)) == "Ks"
   end
 
   test "trump with the left bauer before the right bauer" do
-    played_cards = [{"hearts", "9"}, {"hearts", "10"}, {"hearts", "J"}]
-    hand = [
-      {"spades", "J"},
-      {"clubs", "J"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"spades", "J"}
+    assert choose("c", ~w(9h 10h Jh), ~w(Js Jc Jd Ad Qd)) == "Js"
   end
 
   test "do not trump your partner's good trick" do
-    played_cards = [{"hearts", "9"}, {"hearts", "J"}, {"hearts", "10"}]
-    hand = [
-      {"diamonds", "9"},
-      {"clubs", "9"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"diamonds", "9"}
+    assert choose("c", ~w(9h Jh 10h), ~w(9d 9c Jd Ad Qd)) == "9d"
   end
 
   test "do not trump your partner's good trick when partner lead is winning" do
-    played_cards = [{"hearts", "A"}, {"hearts", "10"}]
-    hand = [
-      {"diamonds", "9"},
-      {"clubs", "9"},
-      {"diamonds", "J"},
-      {"diamonds", "A"},
-      {"diamonds", "Q"}
-    ]
-    result = Ai.choose_card("clubs", played_cards, hand)
-    assert result == {"diamonds", "9"}
+    assert choose("c", ~w(Ah 10h), ~w(9d 9c Jd Ad Qd)) == "9d"
   end
 end
